f99cbda80c24d58672ff2b74cc8e5c4d62f9cf95
aether <mocker26@up.edu>
2023-03-21 16:30:34 -0700

    instance vars

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -3,4 +3,17 @@ package edu.up.cs301.pig;
 import edu.up.cs301.game.infoMsg.GameState;
 
 public class PigGameState extends GameState {
+
+    private int playerID;
+    private int player0Score;
+    private int player1Score;
+    private int runningTotalScore;
+    private int dieValue;
+
+    public PigGameState() {
+
+    }
+
+
+
 }
